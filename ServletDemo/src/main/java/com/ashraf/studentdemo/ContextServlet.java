67588d2cc45ba818e8028e6f9970c59716e747ec
Ashraf Patel <4shr4f@live.co.uk>
2021-11-26 21:55:41 +0000

    added some Jaava Servlet demos with JSTL tags

---
+++ ServletDemo/src/main/java/com/ashraf/studentdemo/ContextServlet.java
@@ -0,0 +1,56 @@
+package com.ashraf.studentdemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContextServlet
+ */
+@WebServlet("/ContextServlet")
+public class ContextServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ContextServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		ServletContext context = getServletContext();
+		
+		String name = context.getInitParameter("Student Name");
+		String languages =  context.getInitParameter("Favourite Languages");
+		
+		out.println("<html><body>");
+		out.println("Name: " + name);
+		out.println("Favourite Languages: " + languages);
+		out.println("</body></html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
